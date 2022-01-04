036b6a14f950cca00393134d649fafdb9446eb93
Elijah <eprince@ah4r.com>
2022-01-03 16:20:10 -0800

    Mockup event bus

---
+++ event-bus/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/event", (req, res) => {
+  const event = req.body;
+
+  // Posts service
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err);
+  });
+
+  // Comments Service
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err);
+  });
+
+  // Query Service
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err);
+  });
+
+  res.send({ status: "ok" });
+});
+
+app.listen(4005, () => {
+  console.log("listening on 4005");
+});
